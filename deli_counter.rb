50c3086a44dc0482e39410fe76565bfd371b7d62
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-16 23:54:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.length == 0
   puts "The line is currently empty."
       else
-    puts = "The line is currently:" 
+    puts = "The line is currently:"
     array.each do |person|
   puts "#{counter}. #{person}"
       counter +=1
