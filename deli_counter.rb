970b244ced5895ff80d421c0a5d43b39f1f2347f
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-16 23:22:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(array)
   if array.length == 0
   puts "The line is currently empty."
       else
-        print "line is currently:"
+        puts "line is currently:" = phrase
         array.each do |person|
-      print "#{counter}. #{person}"
+      phrase<<"#{counter}. #{person}"
       counter +=1
     end
   end
